5baaa42ee772cfdc47233bf5ed145a511366c325
Marcin Kozlowski <marcinguy@gmail.com>
2018-05-11 16:08:24 +0200

    Update client.py
    Added Accept header to be customizeable

--- graphqlclient/client.py
+++ graphqlclient/client.py
@@ -5,12 +5,17 @@ class GraphQLClient:
     def __init__(self, endpoint):
         self.endpoint = endpoint
         self.token = None
+        self.accept = None
 
     def execute(self, query, variables=None):
         return self._send(query, variables)
 
     def inject_token(self, token):
         self.token = token
+    
+    def inject_accept(self, accept):
+        self.accept = accept
+    
 
     def _send(self, query, variables):
         data = {'query': query,
@@ -20,6 +25,9 @@ class GraphQLClient:
 
         if self.token is not None:
             headers['Authorization'] = '{}'.format(self.token)
+        
+        if self.accept is not None:
+            headers['Accept'] = '{}'.format(self.accept)
 
         req = urllib.request.Request(self.endpoint, json.dumps(data).encode('utf-8'), headers)
 
